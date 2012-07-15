63899d6ac2c35594d8d1e2f3dc5bf0afa1f30c08
Integtity <7777js@gmail.com>
2012-07-15 11:11:43 +0800

    WebUI
    前端框架

---
+++ WebUI/js/main.js
@@ -0,0 +1,25 @@
+/* Author:Integ
+*  FSM Web Uesr Interface 2012-7-15
+*/
+ $(function() {
+	$('#nav1').click( function () {
+			$('.active').hide();
+			$('#sec1').show().addClass('active');
+		}
+	);
+	$('#nav2').click( function () {
+			$('.active').hide();
+			$('#sec2').show().addClass('active');
+		}
+	);
+	$('#nav3').click( function () {
+			$('.active').hide();
+			$('#sec3').show().addClass('active');
+		}
+	);
+	$('#nav4').click( function () {
+			$('.active').hide();
+			$('#sec4').show().addClass('active');
+		}
+	);
+ });
